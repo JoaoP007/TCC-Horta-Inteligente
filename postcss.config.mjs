d992f8459edd206ce31c9342260e3943ab1ba8f7
JoaoP007 <84014266+JoaoP007@users.noreply.github.com>
2025-11-04 19:24:30 -0300

    Update PostCSS configuration to use @tailwindcss/postcss
    Removed tailwindcss and autoprefixer plugins, added @tailwindcss/postcss.

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,6 @@
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
-
+// postcss.config.mjs
 export default {
   plugins: {
-    tailwindcss,
-    autoprefixer,
+    "@tailwindcss/postcss": {},
   },
 };
